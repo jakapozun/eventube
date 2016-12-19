cca876910d357661346030faae453832dcc011e2
jakapozun <jaka.pozun2@gmail.com>
2016-12-19 11:57:59 +0100

    contact
    hh

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::post('/profile', 'ProfileController@add_avatar');
 Route::get('/add_event', 'EventController@show_add_event');
 
 Route::post('/insert_event', 'EventController@insert_event');
+
+Route::get('/contact', 'HomeController@index');
+
